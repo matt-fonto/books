bd56ff2160ef9939895e0aa9a255efd11f530bb2
matt-fonto <mateus.fonto@gmail.com>
2025-09-25 18:45:23 +0200

    big o course: binary search notes

--- beyond_cracking_the_coding_interview/part 6 - Principles of coding interviews/big_o_notation_course/binrary_search_recursive.js
+++ beyond_cracking_the_coding_interview/part 6 - Principles of coding interviews/big_o_notation_course/binrary_search_recursive.js
@@ -20,3 +20,60 @@ function binarySearchRecursive(target, arr, left = 0, right = arr.length - 1) {
     return binarySearchRecursive(target, arr, left, midIndex - 1);
   }
 }
+
+/* 
+scenario 1:
+   ->  0  1   2  3   4   5  6
+arr = [1, 5, 10, 15, 20, 25, 30]
+      left
+                            right
+                midIndex
+target = 25
+
+level 1:
+left = 0
+right = 7
+midIndex = floor(0 + 7) / 2 = 3
+arr[midIndex] = 15
+
+15(midIndex) < 25(target) => we must get rid of left 
+            remove  | maintain
+arr = [1, 5, 10, 15 | 20, 25, 30]
+                    left (mid + 1)
+                                right
+                        midIndex
+
+level 2:
+left = midIndex + 1 = 4
+right = 7
+midIndex = floor(4 + 7) / 2 = 5
+arr[midIndex] = 25 -> result
+
+==========
+scenario 2:
+        0 1 ...                    ...8.  9.  10 ...            13
+arr = [1, 5, 10, 15, 20, 25, 30, 35, 50, 100, 200, 300, 1000, 100000]
+      left
+                                                                right
+                            midIndex
+target = 20
+
+level 1:
+left = 0
+right = 13
+midIndex = floor(0 + 13) / 2 = 6
+arr[midIndex] = 30
+
+30 > target => we must get rid of right
+        maintain            | remove
+arr = [1, 5, 10, 15, 20, 25 | 30, 35, 50, 100, 200, 300, 1000, 100000]
+    left
+                        right (mid -1)
+            midIndex 
+
+level 2:
+left = 0
+right = 5
+midIndex = floor(0 + 5) / 2 = 2
+arr[midIndex] = 10
+*/
